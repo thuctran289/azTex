b935cde58024037cf58a0c0700f55bfda3d1bc70
Thuc <thuc@T.Tran>
2015-04-15 21:18:53 -0400

    Prep to pull

--- InternalRepresentation.py
+++ InternalRepresentation.py
@@ -1,25 +1,18 @@
-class InternalRepresentation:
+class InternalRepresentation(object):
 	""" Contains the internal representation for the document and how it looks"""
 
 	def __init__(self):
 		self.internal_elements = []
 
-	def add_line(self, list_of_elements):
-		self.internal_elements.append(list_of_elements)
-
-	def get_line(self, line_number):
-		return self.internal_elements[line_number]
-
 	def get_list(self):
 		return self.internal_elements
 
-	def get_element(self,line_number, element_number):
-		return self.internal_elements[line_number][element_number]
+	def get_element(self, element_number):
+		return self.internal_elements[element_number]
 
-	def add_element(self, element, line_number = None):
-		if line_number == None:
-			self.internal_elements.append(element)
-		else:
-			self.internal_elements[line_number].append(element)
+	def add_element(self, element):
+		self.internal_elements.append(element)
 
+	def __str__(self):
+		return str(self.internal_elements)
 

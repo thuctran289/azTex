3f6c0ff60aeedead9cd385926a74465a38f5d9f9
Jordan Van Duyne <jordannn808@gmail.com>
2015-04-22 17:27:39 -0400

    Make function in AztexRunner

--- AztexRunner.py
+++ AztexRunner.py
@@ -1,52 +1,69 @@
 import sys
 from Element import Element
 from LatexOutput import *
-
-if len(sys.argv) > 1:
-	args = sys.argv[1:]
-	if '.txt' in args:
-		filename = args
-		f = open(filename, 'r')
-		text = f.read()
-	else:
-		text = ' '.join(args)
-else:
-	filename = "input.txt"
-	f = open(filename, 'r')
-	text = f.read()
-
-print "===== ORIGINAL FILE =====\n"
-print text
-print "=========================\n\n"
-
 from Tokenizer import Tokenizer
 from Parser import Parser
 
-tokenizer = Tokenizer(text)
-parser = Parser()
+def run_test(text):
+	tokenizer = Tokenizer(text)
+	parser = Parser()
 
-elements = []
-block = tokenizer.get_next_block()
-while block:
-    element = parser.parseBlock(block)
-    elements.append(element)
-    block = tokenizer.get_next_block()
+	elements = []
+	block = tokenizer.get_next_block()
+	while block:
+		element = parser.parseBlock(block)
+		elements.append(element)
+		block = tokenizer.get_next_block()
 
-print "===== DOCUMENT ELEMENTS ====="
-A = LatexOutput()
+	A = LatexOutput()
+	return '\n'.join(A.to_doc(elements))
 
-#for element in elements:
-#	print A.to_code(element)
-#	print ""
-for element in  A.to_doc( elements):
-	print element
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		args = sys.argv[1:]
+		if '.txt' in args:
+			filename = args
+			f = open(filename, 'r')
+			text = f.read()
+		else:
+			text = ' '.join(args)
+	else:
+		filename = "input.txt"
+		f = open(filename, 'r')
+		text = f.read()
 
-print "==============================\n\n"
+	print "===== ORIGINAL FILE =====\n"
+	print text
+	print "=========================\n\n"
+
+	from Tokenizer import Tokenizer
+	from Parser import Parser
+
+	tokenizer = Tokenizer(text)
+	parser = Parser()
+
+	elements = []
+	block = tokenizer.get_next_block()
+	while block:
+	    element = parser.parseBlock(block)
+	    elements.append(element)
+	    block = tokenizer.get_next_block()
+
+	print "===== DOCUMENT ELEMENTS ====="
+	A = LatexOutput()
+
+	#for element in elements:
+	#	print A.to_code(element)
+	#	print ""
+	for element in  A.to_doc( elements):
+		print element
+
+	print "==============================\n\n"
 
 
-print "==============="
-print "=== SUCCESS ==="
-print "==============="
+	print "==============="
+	print "=== SUCCESS ==="
+	print "==============="
 
 
 

40737c89c0df8a3d2d58b9d26b1437a895000fcb
Isaac Getto <isaac.getto@gmail.com>
2015-05-03 15:39:57 -0400

    move src/AztexRunner.py to main.py

--- main.py
+++ main.py
@@ -1,67 +1,74 @@
+"""
+	@authors: idgetto, jovanduy, thuctran289
+
+	Main aztex program. Run this program to get the LaTeX 
+	output of given aztex code. Creates a .tex file with the
+	same name as the input file (if input is text rather than 
+	a file, creates a file named newfile.tex) with the analogous
+	LaTeX code.
+
+	To run:
+		with a certain file filename: python main.py filename.txt
+		with a file input.txt: python main.py
+		with "just a few words": python main.py "just a few words"
+"""
+
 import sys
 from os import path
+
 here = path.abspath(path.dirname(__file__))
-sys.path.append(here + "/src/") 
+sys.path.append(here + "/src") 
+
 from Element import Element
-import sys
-from Element import Element
-from LatexOutput import *
+from LatexOutput import LatexOutput
 from Tokenizer import Tokenizer
 from Parser import Parser
+from AztexCompiler import AztexCompiler
+
+def is_text_file(filename):
+	return filename.endswith('.txt')
+
+def tex_file_name(txt_file):
+	name = txt_file.split('.')[0]
+	tex_file = name + '.tex'
+	return tex_file
+
+def input_text(argv):
+	inputf = input_file(argv)
+
+	if inputf:
+		f = open(inputf, 'r')
+		return f.read()
+	else:
+		text = ' '.join(argv[1:])
+		return text
+
+def input_file(argv):
+	# no input given
+	if len(argv) == 1:
+		return "input.txt"
+
+	# input file given
+	elif is_text_file(argv[1]):
+		return argv[1]
+
+def output_file(argv):
+	inputf = input_file(argv)
+
+	if inputf:
+		return tex_file_name(inputf)
+	else:
+		return 'out.tex'
 
 def main():
+	md_text = input_text(sys.argv)
+	compiler = AztexCompiler()
+	latex_str = compiler.compile(md_text)
 
-	if len(sys.argv) > 1:
-		args = sys.argv[1:]
-		if '.txt' in args[0]:
-			filename = args[0]
-			w = open(filename[:-4:] + ".tex", 'w')
-			f = open(filename, 'r')
-			text = f.read()
-		else:
-			w = open('newfile.tex', 'w')
-			text = ' '.join(args)
-	else:
-		filename = "input.txt"
-		w = open(filename[:-4:] + ".tex", 'w')
-		f = open(filename, 'r')
-		text = f.read()
+	out_file = output_file(sys.argv)
+	w = open(out_file, 'w')
 
-	print "===== ORIGINAL FILE =====\n"
-	print text
-	print "=========================\n\n"
-
-	from Tokenizer import Tokenizer
-	from Parser import Parser
-
-	tokenizer = Tokenizer(text)
-	parser = Parser()
-
-	elements = []
-	block = tokenizer.get_next_block()
-	while block:
-	    element = parser.parseBlock(block)
-	    elements.append(element)
-	    block = tokenizer.get_next_block()
-
-
-	print "===== DOCUMENT ELEMENTS ====="
-	A = LatexOutput()
-
-	#for element in elements:
-	#	print element
-	#	print ""
-
-	for element in  A.to_doc(elements):
-		w.write(str(element))
-		w.write('\n')
-
-	print "==============================\n\n"
-
-
-	print "==============="
-	print "=== SUCCESS ==="
-	print "==============="
+	w.write(latex_str)
 
 if __name__ == "__main__":
 	main()

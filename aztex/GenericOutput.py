28d14a6f531882b9de371081d200949b6b8353a9
Isaac Getto <isaac.getto@gmail.com>
2015-05-05 20:22:37 -0400

    comment GenericOutput

--- aztex/GenericOutput.py
+++ aztex/GenericOutput.py
@@ -5,12 +5,15 @@ class GenericOutput(object):
 	"""generates an output document given some internal respresentation"""
 
 	def doc_header(self):
+		""" returns the string that should precede the document """
 		pass
 
 	def doc_footer(self):
+		""" returns the string that should follow the document """
 		pass
 
 	def generate_doc(self, representation):
+		""" generates a string out of the representation """
 		doc = ""
 		doc += self.doc_header()
 
@@ -36,6 +39,7 @@ class GenericOutput(object):
 			return ''.join(els)
 
 	def element_dict(self):
+		""" returns the dictionary of elements and functions """
 		return { UnorderedListElement: 		self.unordered_list,
 				 OrderedListElement: 		self.ordered_list,
 				 TextElement: 				self.text,
